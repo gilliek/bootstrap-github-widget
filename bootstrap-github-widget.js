c82beb2b3fc4d24804bb7d27ad20142849f38537
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-04-27 17:33:55 +0200

    add basic repos info fetching

--- bootstrap-github-widget.js
+++ bootstrap-github-widget.js
@@ -14,6 +14,7 @@
         var defaults = {
             user: '',
             widget: 'repos',
+            classes: [],
             limit: 5
         }
 
@@ -23,8 +24,61 @@
         var $element = $(element),
             element = element;
 
-            plugin.init = function() {
+        var fillElement = function(title, body, content, footer) {
+            var classes = plugin.settings.classes.length > 0 ? ' ' + plugin.settings.classes.join(' ') : '';
+            var panel = '<div class="panel panel-default' + classes + '">';
+            panel += '<div class="panel-heading">' + title + '</div>';
+            panel += body != '' ? '<div class="panel-body">' + body + '</div>' : '';
+            panel += content;
+            panel += footer != '' ? '<div class="panel-footer" style="text-align: right;">' + footer + '</div>' : '';
+            panel += '</div>';
+            $element.html(panel);
+        };
+
+        var fetchFromGithub = function() {
+            var user = plugin.settings.user;
+            var action = plugin.settings.widget;
+
+            $.ajax({
+                url: 'https://api.github.com/users/' + user + '/' + action + '?sort=pushed',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    var limit = plugin.settings.limit;
+
+                    var content = '<ul class="list-group">';
+                    var total = 0;
+
+                    $.each(data, function(k, v) {
+                        if (limit > 0 && total >= limit) {
+                            return;
+                        }
+
+                        content += '<li class="list-group-item">';
+
+                        switch (action) {
+                        case 'repos':
+                            content += '<a href="' + v.html_url + '">' + v.full_name + '</a> \
+                                      <br/>' + v.description;
+                            break;
+                        case 'gists':
+                            // TODO
+                            break;
+                        }
+
+                        total++;
+                    });
+
+                    content += '</ul>';
+
+                    fillElement('GitHub ' + action, '', content, '');
+                }
+            });
+        }
+
+        plugin.init = function() {
             plugin.settings = $.extend({}, defaults, options);
+            fetchFromGithub();
         }
 
         plugin.init();
@@ -45,6 +99,7 @@
     $(function() {
         $.each($('div.github-widget'), function() {
             var inputWidget = $(this).data('ghwidget');
+            var inputClasses = $(this).data('classes');
             var inputLimit = $(this).data('limit');
 
             if (inputWidget !== undefined) {
@@ -53,7 +108,8 @@
                 var options = {};
                 options.user = tmp[0];
                 options.widget = tmp[1];
-                options.limit = (inputLimit !== undefined) ? parseInt(inputLimit) : -1;
+                options.classes = (inputClasses !== undefined) ? inputClasses.split(' ') : [];
+                options.limit = (inputLimit !== undefined) ? parseInt(inputLimit) : 5;
 
                 var plugin = new $.githubWidget(this, options);
             }
